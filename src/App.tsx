cfbc7f62e8d642979e51ec68a068920a5643d584
tonym <tony@fbpllc.com>
2022-11-06 13:05:53 -0800

    initial app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import CssBaseline from '@mui/material/CssBaseline';
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      content goes here
+    </div>
+  );
+}
+
+export default App;
