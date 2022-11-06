0539db74db049e10bc77b0a86b57f351084de268
tonym <tony@fbpllc.com>
2022-11-06 14:20:39 -0800

    add time display

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { AppBar, Container, CssBaseline, Toolbar, Typography } from '@mui/material';
+import { useState } from 'react';
+import { format } from 'date-fns';
+import { AppBar, Card, CardContent, Container, CssBaseline, Grid, Toolbar, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const AppWrapper = styled('div')`
@@ -6,17 +8,37 @@ const AppWrapper = styled('div')`
 `;
 
 function App() {
+  const [date] = useState<Date>(new Date());
+  const [timeFormat] = useState<string>('HH:mm:ss');
+  const [dateFormat] = useState<string>('d MMMM yyyy');
+
   return (
     <AppWrapper className="App">
       <CssBaseline />
-      <AppBar position="fixed">
+      <AppBar>
         <Toolbar>
-          <Typography variant="h6" component="div">
+          <Typography component="div" variant="h6">
             Quiz
           </Typography>
         </Toolbar>
       </AppBar>
-      <Container>content goes here</Container>
+      <Container>
+        <Typography align="center" paragraph variant="h6">
+          Session start time:
+        </Typography>
+        <Grid container justifyContent="center">
+          <Card variant="outlined">
+            <CardContent>
+              <Typography component="span" fontWeight="700" variant="h2">
+                {format(date, timeFormat)}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Typography align="center" paragraph variant="h6">
+          {format(date, dateFormat)}
+        </Typography>
+      </Container>
     </AppWrapper>
   );
 }
